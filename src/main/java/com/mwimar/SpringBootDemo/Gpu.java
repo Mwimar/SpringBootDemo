92b02fb3c5611e05fe2158669b028b6734efce5e
Mark Mwima <markmwimar@gmail.com>
2024-08-17 00:09:45 +0300

    autowiring cpu class

--- src/main/java/com/mwimar/SpringBootDemo/Gpu.java
+++ src/main/java/com/mwimar/SpringBootDemo/Gpu.java
@@ -1,10 +1,16 @@
 package com.mwimar.SpringBootDemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Gpu {
+
+    @Autowired
+    Cpu cpu;
+
     public void start(){
+        cpu.process();
 
     }
 }
--- src/main/java/com/mwimar/SpringBootDemo/SpringBootDemoApplication.java
+++ src/main/java/com/mwimar/SpringBootDemo/SpringBootDemoApplication.java
@@ -10,9 +10,11 @@ public class SpringBootDemoApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SpringBootDemoApplication.class, args);
 
-		Alien obj = context.getBean(Alien.class);
-		obj.code();
+//		Alien obj = context.getBean(Alien.class);
+//		obj.code();
 
+		Gpu obj1 = context.getBean(Gpu.class);
+		obj1.start();
 
 
 
