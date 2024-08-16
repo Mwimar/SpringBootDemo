96161c35e4c4193d6b8c598f2c7027b529c70b29
Mark Mwima <markmwimar@gmail.com>
2024-08-16 23:53:19 +0300

    autowire

--- src/main/java/com/mwimar/SpringBootDemo/SpringBootDemoApplication.java
+++ src/main/java/com/mwimar/SpringBootDemo/SpringBootDemoApplication.java
@@ -10,10 +10,9 @@ public class SpringBootDemoApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SpringBootDemoApplication.class, args);
 
-//		Alien obj = context.getBean(Alien.class);
-//		obj.code();
-		Laptop obj = context.getBean(Laptop.class);
-		obj.compile();
+		Alien obj = context.getBean(Alien.class);
+		obj.code();
+
 
 
 	}
