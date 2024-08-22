45aa5d828e6c9d1a36a0c86b748851231109907f
Mark Mwima <markmwimar@gmail.com>
2024-08-22 21:29:53 +0300

    saving object in repo

--- src/main/java/com/mwimar/SpringBootDemo/LaptopRepository.java
+++ src/main/java/com/mwimar/SpringBootDemo/LaptopRepository.java
@@ -1,9 +1,11 @@
 package com.mwimar.SpringBootDemo;
 
 import com.mwimar.SpringBootDemo.model.Laptop;
+import org.springframework.stereotype.Component;
 
+@Component
 public class LaptopRepository {
     public void save(Laptop lap){
-        System.out.println("Saving laptop...");
+        System.out.println("Saved in Database...");
     }
 }
--- src/main/java/com/mwimar/SpringBootDemo/service/LaptopService.java
+++ src/main/java/com/mwimar/SpringBootDemo/service/LaptopService.java
@@ -1,12 +1,19 @@
 package com.mwimar.SpringBootDemo.service;
 
+import com.mwimar.SpringBootDemo.LaptopRepository;
 import com.mwimar.SpringBootDemo.model.Laptop;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LaptopService {
+
+    @Autowired
+    private LaptopRepository repo;
+
     public void addLaptop(Laptop lap){
         System.out.println("Add Lap Method called...");
+        repo.save(lap);
 
     };
 
