faf20d8c6dbef7cd7ffb77a58058f1ec8a3a1631
Mark Mwima <markmwimar@gmail.com>
2024-08-22 21:23:55 +0300

    laptop repository

---
+++ src/main/java/com/mwimar/SpringBootDemo/LaptopRepository.java
@@ -0,0 +1,9 @@
+package com.mwimar.SpringBootDemo;
+
+import com.mwimar.SpringBootDemo.model.Laptop;
+
+public class LaptopRepository {
+    public void save(Laptop lap){
+        System.out.println("Saving laptop...");
+    }
+}
