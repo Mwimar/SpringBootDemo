aee19268225f59705c86640a6a75b73aacdb7484
Mark Mwima <markmwimar@gmail.com>
2024-08-22 20:35:41 +0300

    getters and setters

--- src/main/java/com/mwimar/SpringBootDemo/Alien.java
+++ src/main/java/com/mwimar/SpringBootDemo/Alien.java
@@ -5,11 +5,29 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Alien {
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Computer getCom() {
+        return com;
+    }
 
     @Autowired
+    public void setCom(Computer com) {
+        this.com = com;
+    }
+
+    private int age;
+
+
     private Computer com;
 
     public void code(){
-//        laptop.compile();
+        com.compile();
     };
 }
