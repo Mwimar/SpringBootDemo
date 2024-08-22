735b220257c3f7ac839b78b815d5318a3ae972aa
Mark Mwima <markmwimar@gmail.com>
2024-08-22 20:41:58 +0300

    Desktop class

--- src/main/java/com/mwimar/SpringBootDemo/Laptop.java
+++ src/main/java/com/mwimar/SpringBootDemo/Laptop.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Component;
 public class Laptop implements Computer {
 
     public void compile(){
-
+        System.out.println("Compiling with laptop...");
     }
 }
