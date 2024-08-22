7534b2aa9b377e4e3613eb4185f7461dedb7ac73
Mark Mwima <markmwimar@gmail.com>
2024-08-22 21:06:11 +0300

    laptop instance

--- src/main/java/com/mwimar/SpringBootDemo/SpringBootDemoApplication.java
+++ src/main/java/com/mwimar/SpringBootDemo/SpringBootDemoApplication.java
@@ -1,6 +1,7 @@
 package com.mwimar.SpringBootDemo;
 
 import com.mwimar.SpringBootDemo.model.Alien;
+import com.mwimar.SpringBootDemo.model.Laptop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,10 +12,11 @@ public class SpringBootDemoApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SpringBootDemoApplication.class, args);
 
-Alien obj = context.getBean(Alien.class);
-		System.out.println(obj.getAge());
-obj.code();
+//     Alien obj = context.getBean(Alien.class);
+//	 	System.out.println(obj.getAge());
+//         obj.code();
 
+		Laptop lap = context.getBean(Laptop.class);
 //		Gpu obj1 = context.getBean(Gpu.class);
 //		obj1.start();
 
