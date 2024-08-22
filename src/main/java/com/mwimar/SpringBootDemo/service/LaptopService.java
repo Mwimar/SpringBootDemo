08e4d5cf261fd59d9140e6ceb06c728288f497cd
Mark Mwima <markmwimar@gmail.com>
2024-08-22 21:18:03 +0300

    service annotation

--- src/main/java/com/mwimar/SpringBootDemo/service/LaptopService.java
+++ src/main/java/com/mwimar/SpringBootDemo/service/LaptopService.java
@@ -1,9 +1,16 @@
 package com.mwimar.SpringBootDemo.service;
 
 import com.mwimar.SpringBootDemo.model.Laptop;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LaptopService {
     public void addLaptop(Laptop lap){
+        System.out.println("Add Lap Method called...");
 
     };
+
+    public boolean isGoodforProg(Laptop lap){
+        return true;
+    }
 }
